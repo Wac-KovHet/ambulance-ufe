fb12aa949219cadd523a4815fb7b632b184cfa3b
Samuel Hetteš <samuel.hettes@gmail.com>
2024-02-27 12:27:38 +0100

    Change patient name

--- src/components/shettes-ambulance-wl-list/xhettes-ambulance-wl-list.tsx
+++ src/components/shettes-ambulance-wl-list/xhettes-ambulance-wl-list.tsx
@@ -11,7 +11,7 @@ export class xhettesAmbulanceWlList {
   private async getWaitingPatientsAsync() {
     return await Promise.resolve([
       {
-        name: 'Jožko Púčik',
+        name: 'Karol Kryl',
         patientId: '10001',
         since: new Date(Date.now() - 10 * 60).toISOString(),
         estimatedStart: new Date(Date.now() + 65 * 60).toISOString(),
